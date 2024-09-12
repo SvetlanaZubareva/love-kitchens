ca9cb67ccb8a5d73617c09bc6d3ae42b6c6fccf4
Zubareva Svetlana <se.berseneva@gmail.com>
2024-09-12 15:32:21 +0300

    Готов первый слайдер

--- script.js
+++ script.js
@@ -1,55 +1,21 @@
-import Swiper from './vendor/package/swiper-bundle.min.mjs';
-
-// код для первого слайдера
-
-const swiperContainer = document.querySelector('.swiper');
-const swiperContainer2 = document.querySelector('.swiper2');
-
-document.addEventListener(
-    'DOMContentLoaded',
-    () => {
-      debugger
-        slider();
-        slider2();
-    },
-    true
-)
-
-const slider = () => {
-  if (!swiperContainer) {
-      return ;
-  };
-
-  const swiper = new Swiper(swiperContainer, {
-    direction: 'horizontal',
-    slidesPerView: 3,
-    observer: true,
-    spaceBetween: 15,
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
     loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    }
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
   });
-}
-
-// код для второго слайдера
-const slider2 = () => {
-  if (!swiperContainer2) {
-      return ;
-  };
-
-  const swiper = new Swiper(swiperContainer2, {
-    direction: 'horizontal',
-    slidesPerView: 7,
-    observer: true,
-    spaceBetween: 15,
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next2',
-      prevEl: '.swiper-button-prev2',
-    }
-  });
-}
-
-export {slider};
