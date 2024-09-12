671c15e5ef72017c8f1767e2f09945cfa033e89a
Zubareva Svetlana <se.berseneva@gmail.com>
2024-09-12 16:27:34 +0300

    немного осталось

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const swiper = new Swiper('.swiper', {
   
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
     },
   
     // And if we need scrollbar
